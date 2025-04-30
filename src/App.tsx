fec898f8841db3d85ff4e08ca7923850d4b8d989
miseullang <miseullang@gmail.com>
2025-04-30 22:07:43 +0900

    :memo:Chore: 퍼블리싱 수정용 mock API 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import { BrowserRouter } from 'react-router-dom';
 import Router from './routes/Router';
 import { Toast } from '@components/Toast';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { enableMockAPIs } from './mocks/mockApis';
+
+// 개발 환경에서만 모킹 API 활성화
+if (process.env.NODE_ENV === 'development') {
+  enableMockAPIs();
+}
 
 function App() {
   const queryClient = new QueryClient();
