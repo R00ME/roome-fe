18548e20900ab4b26265b45825e2cb6c64e7db50
miseullang <miseullang@gmail.com>
2025-05-02 10:44:01 +0900

    📝 Chore: 결제 관련 페이지 라우팅 주석 처리

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -13,10 +13,10 @@ import ProfileCardEditPage from '@pages/profile-card-edit/ProfileCardEditPage';
 import Redirection from '@pages/login/components/Redirection';
 import EventPage from '@pages/event/EventPage';
 import PointPage from '@pages/point/PointPage';
-import PaymentPage from '@pages/payment/PaymentPage';
-import PaymentSuccessPage from '@pages/payment/PaymentSuccessPage';
-import PaymentFailPage from '@pages/payment/PaymentFailPage';
-import RefundPage from '@pages/payment/RefundPage';
+// import PaymentPage from '@pages/payment/PaymentPage';
+// import PaymentSuccessPage from '@pages/payment/PaymentSuccessPage';
+// import PaymentFailPage from '@pages/payment/PaymentFailPage';
+// import RefundPage from '@pages/payment/RefundPage';
 import OnboardingPage from '@pages/onboarding/OnboardingPage';
 
 const Router = () => {
@@ -56,14 +56,14 @@ const Router = () => {
           path='/point/:userId'
           element={<PointPage />}
         />
-        <Route
+        {/* <Route
           path='/payment'
           element={<PaymentPage />}
         />
         <Route
           path='/payment/refund'
           element={<RefundPage />}
-        />
+        /> */}
         <Route
           path='/event'
           element={<EventPage />}
@@ -92,14 +92,14 @@ const Router = () => {
         path='/login'
         element={<LoginPage />}
       />
-      <Route
+      {/* <Route
         path='/payment/success'
         element={<PaymentSuccessPage />}
       />
       <Route
         path='/payment/fail'
         element={<PaymentFailPage />}
-      />
+      /> */}
       <Route
         path='/onboarding'
         element={<OnboardingPage />}
