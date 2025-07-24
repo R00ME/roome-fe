6d86b74aea28f0a472b6441d08a8b663345a2760
miseullang <miseullang@gmail.com>
2025-07-25 01:07:01 +0900

    🔨 Refactor: 검색 관련 반응형 컴포넌트 및 스타일링 코드 추가 / 검색 로직 분리

--- src/components/search-modal/SearchItem.tsx
+++ src/components/search-modal/SearchItem.tsx
@@ -1,33 +1,43 @@
-import { SEARCH_THEME } from '@/constants/searchTheme';
-import { truncateTitle } from '@/utils/truncate';
-import { toKoreanDate } from '@utils/dateFormat';
+import { useWindowSize } from '@/hooks/useWindowSize';
+import { DesktopSearchItem } from './SearchItem/DesktopSearchItem';
+import { MobileSearchItem } from './SearchItem/MobileSearchItem';
 
 interface SearchItemProps {
   item: SearchItemType;
   type: 'CD' | 'BOOK';
   onClick: () => void;
+  isSelected?: boolean;
+  onAdd?: (item: SearchItemType) => void;
 }
 
-export const SearchItem = ({ item, type, onClick }: SearchItemProps) => {
-  const theme = SEARCH_THEME[type];
+const LARGE_SCREEN_WIDTH = 1024;
+
+export const SearchItem = ({
+  item,
+  type,
+  onClick,
+  isSelected = false,
+  onAdd,
+}: SearchItemProps) => {
+  const { width } = useWindowSize();
+  const isDesktop = width >= LARGE_SCREEN_WIDTH;
+
+  if (isDesktop) {
+    return (
+      <DesktopSearchItem
+        item={item}
+        type={type}
+        onClick={onClick}
+        isSelected={isSelected}
+      />
+    );
+  }
+
   return (
-    <div
-      className={`flex items-end p-3 mb-3 rounded-lg cursor-pointer ${theme.searchItemBg}`}
-      onClick={onClick}>
-      <div className='flex-1 min-w-0'>
-        <h3 className={`font-semibold ${theme.searchItemText}`}>
-          {truncateTitle(item.title, 24)}
-        </h3>
-        <p className={`text-sm ${theme.searchItemName} line-clamp-1`}>
-          {type === 'BOOK'
-            ? `${item.author} | ${item.publisher || '출판사 정보 없음'}`
-            : `${item.artist} | ${item.album_title || ''}`}
-        </p>
-      </div>
-      <span
-        className={`ml-4 text-sm ${theme.searchItemText} whitespace-nowrap tabular-nums`}>
-        {toKoreanDate(item.date)}
-      </span>
-    </div>
+    <MobileSearchItem
+      item={item}
+      type={type}
+      onAdd={onAdd!}
+    />
   );
 };
